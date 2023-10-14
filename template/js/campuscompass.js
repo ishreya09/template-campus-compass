704e94c0779029739bb8437b4c8de3f13168b008
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-10-14 18:42:25 +0530

    html template

---
+++ template/js/campuscompass.js
@@ -0,0 +1,24 @@
+/* 
+
+    Javascript required for Campus Compass Template 
+    Copyright (c) 2023 Campus Compass 
+    All rights reserved.
+
+*/
+
+/*
+
+JavaScript Coding Standards:
+
+Use CamelCase: For variable and function names, use camelCase
+
+e.g., myVariableName, calculateTotalAmount().
+
+Indentation: Use consistent indentation (2 or 4 spaces) to improve code readability.
+
+Comments: Include comments to explain complex logic or document your code.
+
+Variables: Declare variables with var, let, or const keywords. Don’t declare variables before you use them.
+
+*/
+
